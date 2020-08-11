c8e2793d30b01e460ad5813e7dfc0c049fdc5c32
Josh Price <josh@alembic.com.au>
2020-08-11 19:32:28 +1000

    Handle API error cases

--- lib/climate_api.ex
+++ lib/climate_api.ex
@@ -8,6 +8,8 @@ defmodule ClimateAPI do
     @host <> url
   end
 
+  def process_response_body("Invalid country code" <> _), do: nil
+
   def process_response_body(body) do
     body
     |> xpath(~x"/list", rainfall: ~x".//annualData/double/text()"fl)
@@ -19,7 +21,14 @@ defmodule ClimateAPI do
 
   @spec average_rainfall(integer(), integer(), String.t()) :: float()
   def average_rainfall(from_year, to_year, country_code) do
-    rainfall_data = get!(path(from_year, to_year, country_code)).body[:rainfall]
-    Enum.sum(rainfall_data) / length(rainfall_data)
+    url = path(from_year, to_year, country_code)
+
+    case get(url) do
+      {:ok, %HTTPoison.Response{status_code: 200, body: %{rainfall: rainfall_data}}} ->
+        {:ok, Enum.sum(rainfall_data) / length(rainfall_data)}
+
+      _ ->
+        {:error, "No rainfall data found"}
+    end
   end
 end
--- test/climate_api_test.exs
+++ test/climate_api_test.exs
@@ -1,15 +1,24 @@
 defmodule ClimateAPITest do
   use ExUnit.Case
+  import ClimateAPI
 
   test "average UK rainfall " do
-    assert ClimateAPI.average_rainfall(1980, 1999, "GBR") == 988.8454972331015
+    assert average_rainfall(1980, 1999, "GBR") == {:ok, 988.8454972331015}
   end
 
   test "average French rainfall " do
-    assert ClimateAPI.average_rainfall(1980, 1999, "FRA") == 913.7986955122727
+    assert average_rainfall(1980, 1999, "FRA") == {:ok, 913.7986955122727}
   end
 
   test "average Egypt rainfall " do
-    assert ClimateAPI.average_rainfall(1980, 1999, "EGY") == 54.58587712129825
+    assert average_rainfall(1980, 1999, "EGY") == {:ok, 54.58587712129825}
+  end
+
+  test "for Great Britain from 1985 to 1995" do
+    assert average_rainfall(1985, 1985, "GBR") == {:error, "No rainfall data found"}
+  end
+
+  test "for Middle Earth" do
+    assert average_rainfall(1980, 1999, "MDE") == {:error, "No rainfall data found"}
   end
 end
