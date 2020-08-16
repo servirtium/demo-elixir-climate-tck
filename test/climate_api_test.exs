9520b736f050c0e49cb4f153d1c41091a687a764
Josh Price <josh@alembic.com.au>
2020-08-16 11:13:23 +1000

    Handle error test cases for playback

--- test/climate_api_test.exs
+++ test/climate_api_test.exs
@@ -1,25 +1,27 @@
 defmodule ClimateAPITest do
   use ExUnit.Case
 
-  test "average UK rainfall " do
-    assert ClimateAPI.average_rainfall(1980, 1999, "GBR") == {:ok, 988.8454972331015}
-  end
+  describe "Call remote server" do
+    test "average UK rainfall" do
+      assert ClimateAPI.average_rainfall(1980, 1999, "GBR") == {:ok, 988.8454972331015}
+    end
 
-  test "average French rainfall " do
-    assert ClimateAPI.average_rainfall(1980, 1999, "FRA") == {:ok, 913.7986955122727}
-  end
+    test "average French rainfall" do
+      assert ClimateAPI.average_rainfall(1980, 1999, "FRA") == {:ok, 913.7986955122727}
+    end
 
-  test "average Egypt rainfall " do
-    assert ClimateAPI.average_rainfall(1980, 1999, "EGY") == {:ok, 54.58587712129825}
-  end
+    test "average Egypt rainfall" do
+      assert ClimateAPI.average_rainfall(1980, 1999, "EGY") == {:ok, 54.58587712129825}
+    end
 
-  test "for Great Britain from 1985 to 1995" do
-    assert ClimateAPI.average_rainfall(1985, 1995, "GBR") ==
-             {:error, "No rainfall data found for years 1985 to 1995"}
-  end
+    test "no data available for Great Britain from 1985 to 1995" do
+      assert ClimateAPI.average_rainfall(1985, 1995, "GBR") ==
+               {:error, "No rainfall data found for years 1985 to 1995"}
+    end
 
-  test "Middle Earth isn't a real place" do
-    assert ClimateAPI.average_rainfall(1980, 1999, "MDE") == {:error, "Invalid country code"}
+    test "Error Middle Earth isn't a real place" do
+      assert ClimateAPI.average_rainfall(1980, 1999, "MDE") == {:error, "Invalid country code"}
+    end
   end
 
   describe "Servirtium playback mode" do
@@ -30,6 +32,50 @@ defmodule ClimateAPITest do
 
       assert ClimateAPI.average_rainfall("localhost:34567", 1980, 1999, "GBR") ==
                {:ok, 988.8454972331015}
+
+      Plug.Cowboy.shutdown(Servirtium.HTTP)
+    end
+
+    test "average French rainfall" do
+      Plug.Cowboy.http(Servirtium, [playback: "test/interactions/rainfall_1980_1999_fra.md"],
+        port: 34568
+      )
+
+      assert ClimateAPI.average_rainfall("localhost:34568", 1980, 1999, "FRA") ==
+               {:ok, 913.7986955122727}
+
+      Plug.Cowboy.shutdown(Servirtium.HTTP)
+    end
+
+    test "average Egyptian rainfall" do
+      Plug.Cowboy.http(Servirtium, [playback: "test/interactions/rainfall_1980_1999_egy.md"],
+        port: 34569
+      )
+
+      assert ClimateAPI.average_rainfall("localhost:34569", 1980, 1999, "EGY") ==
+               {:ok, 54.58587712129825}
+
+      Plug.Cowboy.shutdown(Servirtium.HTTP)
+    end
+
+    test "no data available for Great Britain from 1985 to 1995" do
+      Plug.Cowboy.http(Servirtium, [playback: "test/interactions/rainfall_1985_1995_gbr.md"],
+        port: 34570
+      )
+
+      assert ClimateAPI.average_rainfall("localhost:34570", 1985, 1995, "GBR") ==
+               {:error, "No rainfall data found for years 1985 to 1995"}
+
+      Plug.Cowboy.shutdown(Servirtium.HTTP)
+    end
+
+    test "Error Middle Earth isn't a real place" do
+      Plug.Cowboy.http(Servirtium, [playback: "test/interactions/rainfall_1980_1999_mde.md"],
+        port: 34571
+      )
+
+      assert ClimateAPI.average_rainfall("localhost:34571", 1980, 1999, "MDE") ==
+               {:error, "Invalid country code"}
 
       Plug.Cowboy.shutdown(Servirtium.HTTP)
     end
